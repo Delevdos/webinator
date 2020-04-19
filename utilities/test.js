a9e5235c7b5a6e2f53c13939fc60eb8134a86cd3
Stone River <stone@projectbrink.com>
2020-04-19 16:13:38 -0700

    fixed mocha callback execution

--- utilities/test.js
+++ utilities/test.js
@@ -1,27 +1,27 @@
 class Test {
 
 	After(callback) {
-		return after(callback());
+		return after(() => callback());
 	}
 
 	AfterEach(callback) {
-		return afterEach(callback());
+		return afterEach(() => callback());
 	}
 
 	Before(callback) {
-		return before(callback());
+		return before(() => callback());
 	}
 
 	BeforeEach(callback) {
-		return beforeEach(callback());
+		return beforeEach(() => callback());
 	}
 
 	Suite(name, callback) { 
-		return describe(name, callback()); 
+		return describe(name, () => callback()); 
 	}
 
 	TestCase(name, callback) {
-		return it(name, callback());
+		return it(name, () => callback());
 	}
 
 }
