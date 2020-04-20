257a6c8ed06a475db492cd7954e91502e3ff47dd
Stone River <stone@projectbrink.com>
2020-04-19 21:31:20 -0700

    expanded default page open function

--- page_objects/page.js
+++ page_objects/page.js
@@ -1,21 +1,43 @@
-import {  } from './index';
-import { WebComponent } from '../index';
+import { About } from './index';
+import { API, WebComponent } from '../index';
 
 class Page {
 
 	constructor() {
 		this.UI = Page.UI;
+		this.About = About;
 	}
 
-	open(path = '') {
+	/**
+	 *  If a baseUrl is specified in the config, it will be prepended to the url parameter using node's url.resolve() method
+	 * @param {String} path - Path to navigate to
+	 */
+	open(path = '', view_mode = 'desktop') {
 		browser.url(path);
+
+		const FULL_URL = browser.getUrl();
+		const PAGE_STATUS = API.getPageStatus(FULL_URL);
+
+		if(PAGE_STATUS != 200) {
+			throw new Error(`${FULL_URL} returned ${PAGE_STATUS} code`);
+		}
+
+		switch(view_mode) {
+			case 'desktop':
+				browser.maximizeWindow();
+				break;
+			case 'mobile':
+				browser.setWindowSize(width, height);
+			default:
+				break;
+		}
 	}
 
 }
 
 Page.UI = class {
 
-	static get domElement() { return new WebComponent('//xpath') }
+	static get element() { return new WebComponent('//XPath') }
 
 }
 
