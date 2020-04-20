4d9ca8af68dd0d2a575cc2c311579a4022748320
Stone River <stone@projectbrink.com>
2020-04-19 20:28:53 -0700

    fixed debug spec formatting

--- specs/debug.spec.js
+++ specs/debug.spec.js
@@ -1,17 +1,18 @@
 import { Test } from '../index';
 import Page from '../page_objects/page';
 
-Test.Suite('Debug Suite', function(){
+Test.Suite('Debug Suite', () => {
 
-	Test.Before(function(){
+	Test.Before(() => {
 
 		Page.open();
 
 	});
 
-	Test.TestCase('Test Case 1', function(){
+	Test.TestCase('Test Case 1', () => {
 
 		Console.log('Test has ran successfully');
+
 	});
 
 });
